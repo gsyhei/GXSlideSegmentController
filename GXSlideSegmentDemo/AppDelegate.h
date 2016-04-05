4856a067300b0cb0e53bdbf31902011be68653f0
BQL <Gin@gx.local>
2016-04-05 17:08:33 +0800

    Initial Commit

---
+++ GXSlideSegmentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GXSlideSegmentDemo
+//
+//  Created by  GuoShengyong on 16/4/5.
+//  Copyright © 2016年 bql. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
